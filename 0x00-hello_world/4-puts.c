3b9399d24f2a1c7d28dd8219234dee413cb05bcb
e-ngendo <estherngendo9548@gmail.com>
2023-06-15 12:59:02 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This program uses the puts function
+ * Return: 0 (Indicates a success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
