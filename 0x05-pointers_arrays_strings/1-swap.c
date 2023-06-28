144335210e5da0dfa1a231667d65fe6cba58c2e3
e-ngendo <estherngendo9548@gmail.com>
2023-06-28 06:23:50 -0700

    Daada 1-swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,17 +1,15 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * swap_int - swaps the values of two integers
- * @a: integer to swap
- * @b: integer to swap
+ * @a: first integer to swap
+ * @b: second integer to swap
  */
 
 void swap_int(int *a, int *b)
 
 {
-	int m;
-
-	m = *a;
+	int temp = *a;
 	*a = *b;
-	*b = m;
+	*b = temp;
 }
