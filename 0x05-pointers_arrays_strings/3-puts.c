72cb445833367f3f57b2944d20d937e55e68b191
e-ngendo <estherngendo9548@gmail.com>
2023-06-28 10:57:41 -0700

    mmm

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _puts - Prints a string
@@ -6,12 +7,15 @@
  * Return: void
  */
 
-void _puts(char *str)
+int main(void)
 
 {
+	char *str;
+
 	while (*str != '\0')
 	{
 		_putchar(*str++);
 	}
 		_putchar('\n');
+	return (0)
 }
